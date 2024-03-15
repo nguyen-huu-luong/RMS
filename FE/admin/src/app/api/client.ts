aaf3623436804d7d7d8e287afea320d5b6c68955
nguyen-huu-luong <nguyenhuuluong1204@gmail.com>
2024-03-15 14:27:05 +0700

    [RMS-00][FIX] Integrating customers management page and customer detail page

--- FE/admin/src/app/api/client.ts
+++ FE/admin/src/app/api/client.ts
@@ -3,47 +3,67 @@ import axios from 'axios';
 import { mutate } from 'swr';
 
 export const queryClientInfo = async (token: any, requestBody: object) => {
-  try {
-    const response = await axios.get(`${process.env.BASE_URL}/customers/21`, {
-      headers: {
-        'Authorization': `Bearer ${process.env.TOKEN}`,
-        'Content-Type': 'application/json',
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.error('Error adding to cart:', error);
-    throw error;
-  }
+    try {
+        const response = await axios.get(`${process.env.BASE_URL}/customers/21`, {
+            headers: {
+                'Authorization': `Bearer ${process.env.TOKEN}`,
+                'Content-Type': 'application/json',
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error geting customer info:', error);
+        throw error;
+    }
 };
 
-export const customerFetcher = async (url: string) => {
-  try {
-    let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEiLCJmdWxsTmFtZSI6Ik1hbmFnZXIgTWFuYWdlciIsImVtYWlsIjoiQ291cnRuZXkuUm9tYWd1ZXJhODRAeWFob28uY29tIiwicm9sZSI6Im1hbmFnZXIiLCJpYXQiOjE3MDg4NTM3ODYsImV4cCI6MTcwODk0MDE4Nn0.LG9bEVniTB08gPTX-1dsHgtzu_nJ1sYy2qSvg_SZMMY"
-    const response = await fetch(url, {
-      headers: {
-        'Authorization': `Bearer ${token}`,
-        'Content-Type': 'application/json',
-      },
-    })
-    return response.json();
-  } catch (error) {
-    console.log(error)
-  }
+export const customerFetcher = async (customerId: string, token: any) => {
+    try {
+        let url = `${process.env.BASE_URL}/customers/${customerId}`
+        const response = await fetch(url, {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+        })
+        return response.json();
+    } catch (error) {
+        console.error('Error fetching customer info:', error);
+        throw error;
+    }
 };
 
-export const updateCustomerInfo = async (data: any, cid: any) => {
-  let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEiLCJmdWxsTmFtZSI6Ik1hbmFnZXIgTWFuYWdlciIsImVtYWlsIjoiQ291cnRuZXkuUm9tYWd1ZXJhODRAeWFob28uY29tIiwicm9sZSI6Im1hbmFnZXIiLCJpYXQiOjE3MDg4NTM3ODYsImV4cCI6MTcwODk0MDE4Nn0.LG9bEVniTB08gPTX-1dsHgtzu_nJ1sYy2qSvg_SZMMY"
-  try {
-    const response = await axios.put(`http://localhost:3003/api/customers/${cid}`, data, {
-      headers: {
-        'Authorization': `Bearer ${token}`,
-        'Content-Type': 'application/json',
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.error('Error adding to cart:', error);
-    throw error;
-  }
+export const updateCustomerInfo = async (data: any, cid: any, token: any) => {
+    try {
+        console.log(token)
+        const response = await axios.put(`${process.env.BASE_URL}/customers/${cid}`, data, {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error updating customer info:', error);
+        throw error;
+    }
+}
+
+export const customersFetcher = async (token: any, tableParams: any, sortQueries: any) => {
+    try {
+        console.log(`${process.env.BASE_URL}`)
+        const response = await axios.get(`${process.env.BASE_URL}/customers/all?page=${tableParams.pagination?.current}
+    &pageSize=${tableParams.pagination?.pageSize}${sortQueries}`,
+            {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json',
+                },
+            })
+        return response.data
+
+    } catch (error) {
+        console.error('Error updating customers info:', error);
+        throw error;
+    }
 }
