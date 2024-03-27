13c48ca099395aae68ef7411a50ee1e354b5641d
nguyen-huu-luong <nguyenhuuluong1204@gmail.com>
2024-03-27 18:16:28 +0700

    Create sample data

--- BE/Internal/migrations/20231110080651-create-product.js
+++ BE/Internal/migrations/20231110080651-create-product.js
@@ -18,7 +18,7 @@ module.exports = {
         type: Sequelize.FLOAT
       },
       thumbnails: {
-        type: Sequelize.BLOB
+        type: Sequelize.STRING
       },
       createdAt: {
         allowNull: false,
